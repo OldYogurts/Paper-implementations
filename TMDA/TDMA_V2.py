46b1f70e5b1a0e870e3d250defa4ea476a7456b8
OldYogurts <kasis.antonis@gmail.com>
2026-03-03 12:48:37 +0200

    weighted improvement

--- TMDA/TDMA_V2.py
+++ TMDA/TDMA_V2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import random
+import math
 from dataclasses import dataclass
 import matplotlib.pyplot as plt
 
@@ -43,6 +44,7 @@ class Tx():
 		self.current_buf_state : int = 0
 		self.buffer : list = [];
 		self.packets_transmitted_per_ti : int = 0;
+		self.tx_chosen = 0;
 	def fill_buffer(self,packets : list) -> list:
 		for p in packets:
 			self.buffer.append(p);
@@ -50,7 +52,7 @@ class Tx():
 				raise Exception(f'ERROR : Packet overflow buffer has " {len(self.buffer)} " packets, can hold up to " {self.buf_len} "');
 				break;
 		return self.buffer
-	def remove_packet(self,packet :object) -> None:
+	def send_packet(self,packet :object) -> None:
 		self.buffer.remove(packet);
 		
 
@@ -107,27 +109,110 @@ class Generate_system():
 			self.System_beta[self.System_omega[i]] =[R[c],R[c+1]];
 			c+=2;
 		return self.System_beta;
-	def Make_metrics(self,System) -> object:
+	def Make_metrics(self,System) -> list[object]:
 		for t in self.System_tx:
 			m = Metrics(tx_id = t.tx_id)
 			self.System_metrics.append(m);
 		return self.System_metrics
 # METHODS
 
-def RANDOM_TDMA(System:object , omega:list[str],alpha:dict[str,object]) -> dict[object,str]:
+def RANDOM_TDMA(System:object ,tx, omega:list[str],alpha:dict[str,object]) -> dict[object,str]:
 	assigned = {};
 	chosen= [];	
 	for i in range(System.channels):
 		while(True):
-			txx = np.random.randint(0,System.devices_tx)
-			if alpha[omega[i]][txx] not in chosen:
-				assigned[alpha[omega[i]][txx]] = omega[i];
-				chosen.append(alpha[omega[i]][txx]);
+			t =random.choice(tx);
+			if t  not in chosen:
+				assigned[t] = omega[i];
+				chosen.append(t);
 				#print(alpha[omega[i]][txx].tx_id);
 				break;
 	#print(assigned)
 	return assigned
 
+def g(a,b,c,x):
+	g = ((a*x)/math.log(1+b/(1+x))) +c
+	return g 
+	
+
+def weight_calc(tx):
+	weights = [0,0,0,0,0,0,0,0];
+	i = 0;
+	for t in tx:
+		test = []
+		for p in t.buffer: test.append(p.packet_rx);
+		w =  1+ len(set(test)) + t.current_buf_state;
+		weights[i] =w 
+		i+=1
+	return weights
+			
+def RANDOM_TDMA_weighted(System:object,tx:object , omega:list[str]) -> dict[object,str]:
+	assigned = {};
+	chosen= [];	
+	weights = weight_calc(tx);
+	c_w = [];
+	for i in range(System.channels):
+		while(True):
+			t =random.choices(tx,weights);
+			t = t[0];
+			if t  not in chosen:                       			
+				assigned[t] = omega[i];
+				t.tx_chosen =0;
+				chosen.append(t);                	
+				l = tx.index(t);
+				c_w.append(weights[l]);
+				break;
+	#print(assigned)
+	return assigned
+
+# THIS IS  ABSOLUTE FRESH SHIT 
+#def utility_calc(coallitions):
+#	assigned = {}
+#	for k,v in coallitions.items():
+#		pack_ids = [];
+#		Utila,Utilb=0,0;
+#		
+#		for p in v[0].buffer:
+#			pack_ids.append(p.packet_rx);
+#		Utila = math.log(len(set(pack_ids))+1,10)
+#		for p in v[1].buffer:
+#			pack_ids.append(p.packet_rx);
+#		Utilb = math.log(len(set(pack_ids))+1,10)
+#		if (Utila > Utilb):
+#			assigned[v[0]] = k;
+#		elif(Utila < Utilb):
+#			assigned[v[1]] = k;
+#		else:
+#			m = np.random.randint(0,1);
+#			assigned[v[m]] = k;
+#		print(Utila,Utilb);
+#		
+#	return assigned;
+#
+#def RANDOM_TDMA_impr(System:object,tx , omega:list[str]) -> dict[object,str]:
+#	assigned,collabs,t,chosen,l = {}, {} ,tx ,[] ,0 ;
+#	while(True):
+#		col = [];
+#		for i in range(2):
+#			while(True):
+#				n = np.random.randint(0,System.devices_tx-1);
+#				if n not in chosen:
+#					chosen.append(n);
+#					col.append(n);
+#					break;
+#		collabs[omega[l]] = [tx[col[0]],tx[col[1]]];
+#		l +=1
+#		if len(chosen) >=6:
+#			break;
+#	col = [];		
+#	for i in range(8):
+#		if i not in chosen:
+#			col.append(i)
+#	collabs[omega[l]] = [tx[col[0]],tx[col[1]]];
+#
+#	assigned = utility_calc(collabs);
+#	return assigned
+#
 
 def generate_long_queue(system :object, rx :object)->list[object]:
 
@@ -157,16 +242,15 @@ def GiPi(space:int ,long_queue:list[object],system:object,distribution:str ='poi
 	while (True):
 		packet_number = np.random.poisson(system.poisson);
 		if packet_number <= space: break;
-	for i in range(packet_number):
-		packets.append(long_queue[i]);
-		del long_queue[i];
+	packets= long_queue[:packet_number];
+	del long_queue[:packet_number];
+	
 	#print(packet_number,len(long_queue),packets)
 	return packets;
+	
 
 def REfill_tx_buffers(tx:object,queue:list[object],system:object) -> None:
 	for t in tx:
-
-		#print(t.current_buf_state);
 		space = t.buf_len - t.current_buf_state
 		p = GiPi(space,queue,system)
 		t.buffer += p;
@@ -182,10 +266,11 @@ def STAR(tx:object,avail_rx:dict[str,object],d:dict[object,str]) -> None:
 		rxa = avail_rx[v][0]
 		rxb = avail_rx[v][1]
 		for p in k.buffer:
-			if (p.packet_rx ==rxa.rx_id )  and (not tx_has_send):
+			if (p.packet_rx ==rxa.rx_id )  and ( not tx_has_send ):
+				# send(from_tx,to_rx,packet);
 				send(k,rxa,p);     
 				tx_has_send = True
-			elif (p.packet_rx == rxb.rx_id) and (not tx_has_send):
+			elif (p.packet_rx == rxb.rx_id) and ( not tx_has_send ):
 				send(k,rxb,p);
 				tx_has_send = True
 		
@@ -193,11 +278,11 @@ def STAR(tx:object,avail_rx:dict[str,object],d:dict[object,str]) -> None:
 				p.delay +=1;
 
 
-def send(tx:object,rx:object,p:object) -> None:
-	p.packet_tx = tx.tx_id
-	rx.receive_packet(p);
-	tx.remove_packet(p);
-	tx.packets_transmitted_per_ti +=1;
+def send(from_tx:object,to_rx:object,pack_sending:object) -> None:
+	pack_sending.packet_tx = from_tx.tx_id
+	to_rx.receive_packet(pack_sending);
+	from_tx.send_packet(pack_sending);
+	from_tx.packets_transmitted_per_ti +=1;
 
 
 
@@ -222,8 +307,13 @@ def Calculate_metrics(metrics:list[object],rx:list[object],steps:int) ->list[int
 	delay = 0;
 	for m in metrics:
 		tp +=m.tpi
-		qLen+=m.qi/steps
-		di.append(m.qi/m.tpi)
+		qLen+=m.qi/steps	
+		try:
+			di.append(m.qi/m.tpi)
+		except ZeroDivisionError :
+			m.tpi +=0.0000001
+			di.append(m.qi/m.tpi)
+
 		assert steps == m.ti
 	#tp /=len(metrics);
 	tp /=steps
@@ -235,17 +325,25 @@ def Calculate_metrics(metrics:list[object],rx:list[object],steps:int) ->list[int
 
 
 
-def plotter_scatter(metrics,use_3d = 1):
+def plotter_scatter(metrics,m,use_3d = 1):
 
     plt.figure(figsize=(10, 6))
-    
+    met_w = metrics[:m-1]
+    met_s= metrics[m:]
+	
     if use_3d:
         # 3D mode: [x, y1, y2] format
-        x_coords = [p[0] for p in metrics]
-        y1_coords = [p[1] for p in metrics]
-        y2_coords = [p[2] for p in metrics]
+        x_coords =  [p[0] for p in met_w]
+        y1_coords = [p[1] for p in met_w]
+        y2_coords = [p[2] for p in met_w]
         
-        plt.scatter(x_coords, y1_coords, s=50, alpha=0.7, label='Average packet Delay', marker='o')
+        plt.scatter(x_coords, y1_coords, s=50, alpha=0.7,color='red', label='Average packet Delay, Weighted', marker='o')
+        plt.scatter(x_coords, y2_coords, s=50, alpha=0.7, label='Average Queue Length', marker='s')
+        x_coords =  [p[0] for p in met_s]
+        y1_coords = [p[1] for p in met_s]
+        y2_coords = [p[2] for p in met_s]
+        
+        plt.scatter(x_coords, y1_coords, s=50, alpha=0.7,color='blue' ,label='Average packet Delay,Standard', marker='o')
         plt.scatter(x_coords, y2_coords, s=50, alpha=0.7, label='Average Queue Length', marker='s')
         
     else:
@@ -263,32 +361,6 @@ def plotter_scatter(metrics,use_3d = 1):
     plt.xlim(left=0)
     plt.ylim(bottom=0)
     plt.show()
-#
-#
-
-
-#    if use_3d == 1:
-#
-#	x_coords  = [point[0] for point in metrics]
-#	y1_coords = [point[1] for point in metrics]
-#	y2_coords = [point[2] for point in metrics]
-#
-#        plt.scatter(x_coords, y1_coords, s=50, alpha=0.7, label='Points Y1', marker='o')
-#        plt.scatter(x_coords, y2_coords, s=50, alpha=0.7, label='Points Y2', marker='s')
-#        plt.title('3D Parameter Plot (Dual Y Lines)')
-#    else :
-#	x_coords = [point[0] for point in metrics]
-#	y_coords = [point[1] for point in metrics]
-#	
-#	plt.figure(figsize=(8, 6))
-#	plt.scatter(x_coords, y_coords, s=30, alpha=1)
-#    plt.xlabel('TP')
-#    plt.ylabel('D')
-#    plt.grid(True, alpha=0.3)
-#    plt.xlim(left=0)
-#    plt.ylim(bottom=0),
-#    plt.show()
-#
 
 def make_system(system):
 	g = Generate_system();
@@ -305,25 +377,43 @@ def make_system(system):
 
 
 
-def run_simulation(system,points,steps,min_packets,max_packets):
+def run_simulation(system,points,steps,min_packets,max_packets,imp):
 	setMIN_PACKETS_PER_RX(System3,min_packets/8);
 	setMAX_PACKETS_PER_RX(System3,max_packets/8);
 	met_list = []
 	setSystemL(System3,4);
 	for m in range(1,points):
 		tx,rx,omega,alpha,beta,metrics,l = make_system(System3)
-		setPoisson(System3,(m)*0.01)
+		setPoisson(System3,(m)*0.05)
 		for i in range(steps):
 			#print(f'\n\n {i} \n\n');
-			d =RANDOM_TDMA(System3,omega,alpha);
+			d =RANDOM_TDMA_weighted(System3,tx,omega);
 			REfill_tx_buffers(tx,l,System3);
 			STAR(tx,beta,d);
 			Update_metrics(metrics,tx)
-		met_list.append(Calculate_metrics(metrics,rx,steps));
-	plotter_scatter(met_list);
+		met_list.append(Calculate_metrics(metrics,rx,steps) );
+	m = len(met_list);
+	for m in range(1,points):
+		tx,rx,omega,alpha,beta,metrics,l = make_system(System3)
+		setPoisson(System3,(m)*0.05)
+		for i in range(steps):
+			#print(f'\n\n {i} \n\n');
+			d =RANDOM_TDMA(System3,tx,omega,alpha);
+			REfill_tx_buffers(tx,l,System3);
+			STAR(tx,beta,d);
+			Update_metrics(metrics,tx)
+		met_list.append(Calculate_metrics(metrics,rx,steps) );
 
+	plotter_scatter(met_list,m);
 
 
 if __name__ == '__main__':
-
-	run_simulation(System3,100,1000,15000,30000);
+	run_simulation(System3,20,1000,150000,300000,0);
+#
+#	setMIN_PACKETS_PER_RX(System3,10);
+#	setMAX_PACKETS_PER_RX(System3,100);
+#	setSystemL(System3,4);
+#
+#	tx,rx,omega,alpha,beta,metrics,l = make_system(System3);
+#	RANDOM_TDMA_weighted(System3,tx,omega,alpha)
+#
